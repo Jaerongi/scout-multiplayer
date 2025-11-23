3adf3163fd5711bb70184d0d5ce14283cb0fe4a8
Jaerongi <156269777+Jaerongi@users.noreply.github.com>
2025-11-23 22:13:27 +0900

    Update socket.js

--- public/socket.js
+++ public/socket.js
@@ -1,9 +1,11 @@
-let socket = io();
+window.socket = io();
 
 window.roomId = null;
+window.myPermUid = null;
 
 // 방 입장
 window.enterRoom = function(roomId, userName, permUid) {
   window.roomId = roomId;
   socket.emit("joinRoom", { roomId, userName, permUid });
 };
+
